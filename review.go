6c0264dee5aed51d414cefb9bacf753897ce2497
gustjd887 <gustjd887@gmail.com>
2023-03-18 10:25:51 +0900

    v2 commit

--- review.go
+++ review.go
@@ -3,8 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
+
+type ratings []struct {
+	Id   int `json:"Id"`
+	Star int `json:"Star"`
+}
 
 type reviewes struct {
 	Id       int    `json:"Id"`
@@ -20,23 +26,44 @@ func main() {
 		Star:     0,
 		Reviewer: "Reviewer1",
 		Review:   "An extremely entertaining play by Shakespeare. The slapstick humour is refreshing!",
-		Color:    "",
+		Color:    "black",
 	}
 	reviewer2 := reviewes{
 		Id:       2,
 		Star:     0,
 		Reviewer: "Reviewer2",
 		Review:   "Absolutely fun and entertaining. The play lacks thematic depth when compared to other plays by Shakespeare.",
-		Color:    "",
+		Color:    "black",
 	}
-
-	reviewer := []reviewes{reviewer1, reviewer2}
-	bs, err := json.Marshal(reviewer)
-	if err != nil {
-		fmt.Println(err)
-	}
-
 	http.HandleFunc("/review", func(w http.ResponseWriter, r *http.Request) {
+		resp, err := http.Get("http://localhost:8000/rating")
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s\n", string(data))
+
+		var rat ratings
+		json.Unmarshal(data, &rat)
+
+		for _, v := range rat {
+			if v.Id == 1 {
+				reviewer1.Star = v.Star
+			} else if v.Id == 2 {
+				reviewer2.Star = v.Star
+			}
+		}
+
+		reviewer := []reviewes{reviewer1, reviewer2}
+		bs, err := json.Marshal(reviewer)
+		if err != nil {
+			fmt.Println(err)
+		}
 		w.Write(bs)
 	})
 	http.ListenAndServe(":8001", nil)
